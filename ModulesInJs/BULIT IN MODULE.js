1a551b26a890e477e3b171a6708da4dc06fbb6ed
kandregulahansikha24csm-rgb <kandregulahansikha.24.csm@anits.edu.in>
2026-08-11 12:04:22 +0530

    Add files via upload

---
+++ ModulesInJs/BULIT IN MODULE.js
@@ -0,0 +1,8 @@
+// Built-in module example
+
+const os = require("os");
+
+console.log("Operating System:", os.platform());
+console.log("Architecture:", os.arch());
+console.log("Computer Name:", os.hostname());
+console.log("Free Memory:", os.freemem());
---
+++ ModulesInJs/event.js
@@ -0,0 +1,14 @@
+// event.js
+
+function eventName() {
+    return "Radha Krishna Celebration";
+}
+
+function welcomeDevotee(name) {
+    return "Welcome " + name + " to the Radha Krishna Event!";
+}
+
+module.exports = {
+    eventName,
+    welcomeDevotee
+};
